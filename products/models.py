d77509399356b5e67c8fef29651cbc7c97f05b6c
hideoutoasis <sunbook1024@gmail.com>
2024-04-29 23:44:45 +0900

    basic settings complete

---
+++ products/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField()
+    image = models.ImageField(upload_to = "images/", blank=True)
+    # 로그인 상태는 계정관련 추가한 후에 다시 마이그레이션하자
+    
