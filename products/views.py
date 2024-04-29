88b1dc76e6b1dccbd1dcd920c7eb36875505dda7
hideoutoasis <sunbook1024@gmail.com>
2024-04-30 00:06:22 +0900

    product list

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import ProductSerializer
 
-# Create your views here.
+from .models import Product
+
+class ProductAPIView(APIView):
+    def get(self, request): # 상품목록조회
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
