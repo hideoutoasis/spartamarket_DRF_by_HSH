dc5d875dad9863045125135f79e1632852e6ea64
hideoutoasis <sunbook1024@gmail.com>
2024-04-30 10:46:22 +0900

    CRUD feature prototype done

--- products/views.py
+++ products/views.py
@@ -1,15 +1,49 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.core import serializers
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from .serializers import ProductSerializer
 
 from .models import Product
 
 class ProductAPIView(APIView):
-    def get(self, request): # 상품목록조회
+    def get(self, request): # 상품 목록조회
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+    
+    def post(self, request): # 상품 등록
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ArticleDetailAPIView(APIView):
+    
+    # permission_classes = [ IsAuthenticated ] 일단 계정 기능 만들기 전까지는 주석처리
+    
+    def get_object(self, pk):
+        return get_object_or_404(Product, pk=pk)
+    
+    def get(self, request, pk): # 상세페이지 조회
+        product = self.get_object(pk) 
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    
+    def put(self, request, pk): # 상품 수정
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+        
+    def delete(self, request, pk): # 상품 삭제
+        product = self.get_object(pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
