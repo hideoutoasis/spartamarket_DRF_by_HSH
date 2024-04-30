8105345d3beb45e9e4367ad7e4ccf5dfe2521a56
hideoutoasis <sunbook1024@gmail.com>
2024-04-30 11:46:23 +0900

    products feature fixed

--- products/views.py
+++ products/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-from django.core import serializers
+# from django.http import JsonResponse, HttpResponse
+# from django.core import serializers
 from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
+# from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
