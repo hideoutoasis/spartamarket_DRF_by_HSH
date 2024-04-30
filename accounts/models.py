108362b1ff67adba55904546aa3ad016104fe0c6
hideoutoasis <sunbook1024@gmail.com>
2024-04-30 21:32:01 +0900

    accounts developing

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+
+# 비밀번호 받을 양식 따로 만들어야 함.
 
 class User(AbstractUser):
-    pass
+    
+    # 비밀번호 받는 함수
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
+    
+    username = models.CharField(default='', max_lenght=20, null=False, blank=False, unique=True)
+    email = models.CharField(default='', max_lenght=100, null=False, blank=False, unique=True)
+    name = models.CharField(default='', max_lenght=100, null=False, blank=False)
+    nickname = models.CharField(default='', max_lenght=30, null=False, blank=False, unique=True)
+    birthday = models.CharField(default='', max_lenght=30, null=False, blank=False)
+    
+    # User 모델 관리 코드, 계정 활성화, 사용자 활성화 상태
+    is_active = models.BooleanField(default=True)    
+    is_admin = models.BooleanField(default=False)
+    
+    # 필수입력 설정
+    REQUIRED_FIELDS = ["username", "email", "name", "nickname", "birthday", "password"]
