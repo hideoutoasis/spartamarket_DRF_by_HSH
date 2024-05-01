e38a37f6c83baa982a96793d7a115953eeea5040
hideoutoasis <sunbook1024@gmail.com>
2024-05-01 14:04:37 +0900

    auth feature developing

--- products/models.py
+++ products/models.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
     content = models.TextField()
     image = models.ImageField(upload_to = "images/", blank=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
